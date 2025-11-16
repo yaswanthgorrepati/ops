52649d67d6fa7a3f86ae7a691218006b14352958
yaswanth <gorrepatiyaswanth2@gmail.com>
2025-11-16 11:07:49 +0530

    Repo init

---
+++ src/main/java/com/ecommerce/ops/OrderProcessingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.ops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderProcessingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderProcessingSystemApplication.class, args);
+	}
+
+}
